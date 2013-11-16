761f6be54b6220765ec1553cead2a3aa35e13205
Dave Pederson <david.pederson1@montana.edu>
2013-11-16 14:56:36 -0700

    Remove import

--- src/main/scala/com/dpederson/app/ServerApp.scala
+++ src/main/scala/com/dpederson/app/ServerApp.scala
@@ -4,13 +4,12 @@ package app
 import com.twitter.finagle.builder.ServerBuilder
 import com.twitter.finagle.thrift.ThriftServerFramedCodec
 import hello._
-import java.net.InetSocketAddress
 import org.apache.thrift.protocol.TBinaryProtocol
 
 object ServerApp extends App {
   val processor = HelloServiceProcessor()
   val protocol = new TBinaryProtocol.Factory()
   val service = new HelloService$FinagleService(processor, protocol)
-  ServerBuilder().bindTo(new InetSocketAddress(8001))
+  ServerBuilder().bindTo(new java.net.InetSocketAddress(8001))
     .codec(ThriftServerFramedCodec()).name("hello_server").build(service)
 }
