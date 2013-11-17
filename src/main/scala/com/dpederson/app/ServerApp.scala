0cbed1a9f22197bac0265527d69117b698f5f0f4
Dave Pederson <david.pederson1@montana.edu>
2013-11-16 20:26:33 -0700

    Add slf4j

--- src/main/scala/com/dpederson/app/ServerApp.scala
+++ src/main/scala/com/dpederson/app/ServerApp.scala
@@ -6,10 +6,15 @@ import com.twitter.finagle.thrift.ThriftServerFramedCodec
 import hello._
 import org.apache.thrift.protocol.TBinaryProtocol
 
+/**
+ * Start the hello service on port 8001.
+ */
 object ServerApp extends App {
+
   val processor = HelloServiceProcessor()
   val protocol = new TBinaryProtocol.Factory()
   val service = new HelloService$FinagleService(processor, protocol)
   ServerBuilder().bindTo(new java.net.InetSocketAddress(8001))
     .codec(ThriftServerFramedCodec()).name("hello_server").build(service)
-}
+
+} // ServerApp
--- src/main/scala/com/dpederson/hello/HelloServiceProcessor.scala
+++ src/main/scala/com/dpederson/hello/HelloServiceProcessor.scala
@@ -2,11 +2,20 @@ package com.dpederson
 package hello
 
 import com.twitter.util.Future
+import org.slf4j.{ Logger, LoggerFactory }
 
+/**
+ * The HelloService implementation.
+ */
 object HelloServiceProcessor {
+
+  val logger: Logger = LoggerFactory.getLogger("HelloServiceProcessor")
+
   def apply(): HelloService[Future] = new HelloService[Future] {
-    override def sayHello(msg: HelloMsg): Future[HelloMsg] = Future.value(
-      HelloMsg("Hello, %s!" format msg.name)
-    )
+    override def sayHello(msg: HelloMsg): Future[HelloMsg] = {
+      logger.info(s"Received message: ${msg.name}")
+      Future.value(HelloMsg("Hello, %s!" format msg.name))
+    }
   }
-}
+
+} // HelloServiceProcessor
