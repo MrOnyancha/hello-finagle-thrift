74c4c006ff86bb114f37a7fd59c0791dc7eba176
Dave Pederson <david.pederson1@montana.edu>
2013-11-18 18:45:38 -0700

    Use framed transport in nodejs client

--- clients/nodejs/hello_client.js
+++ clients/nodejs/hello_client.js
@@ -2,8 +2,7 @@ var thrift = require('thrift')
   , ttypes = require('./gen-nodejs/hello_types')
   , HelloService = require('./gen-nodejs/HelloService.js');
 
-var connection = thrift.createConnection('localhost', 8001);
-//var connection = thrift.createConnection('localhost', 8081, {transport: thrift.TFramedTransport});
+var connection = thrift.createConnection('localhost', 8001, {transport: thrift.TFramedTransport});
 var client = thrift.createClient(HelloService, connection);
 
 connection.on('error', function(err) {
